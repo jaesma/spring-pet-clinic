e03e074d42b94947b8c1aedaa471bdbe8ef37832
jaesma <jamesmanalang11392@gmail.com>
2019-08-14 17:15:59 +0800

    Adding base entity

--- src/main/java/com/manalangjames/app/model/Person.java
+++ src/main/java/com/manalangjames/app/model/Person.java
@@ -1,6 +1,6 @@
 package com.manalangjames.app.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- src/main/java/com/manalangjames/app/model/PetType.java
+++ src/main/java/com/manalangjames/app/model/PetType.java
@@ -1,6 +1,6 @@
 package com.manalangjames.app.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
