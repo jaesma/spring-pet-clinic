21f1459d2ff460cf6418b6f3ec327b5099346a77
jaesma <jamesmanalang11392@gmail.com>
2019-08-14 17:42:24 +0800

    Using Generics, implementing Crud Service && custom banner.

--- src/main/java/com/manalangjames/app/services/VetService.java
+++ src/main/java/com/manalangjames/app/services/VetService.java
@@ -2,14 +2,8 @@ package com.manalangjames.app.services;
 
 import com.manalangjames.app.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 
 }
